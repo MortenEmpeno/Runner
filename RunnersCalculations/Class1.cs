6ac5de7ba04affad113411edd2ab328af7a7db82
Morten Empeño <contact@mortenempeno.com>
2016-11-05 06:08:29 +0100

    initial

---
+++ RunnersCalculations/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunnersCalculations
+{
+    public class Class1
+    {
+        public double CalculatedTime(double givenTime, double givenDistance, double distanceToPredict)
+        {
+            return givenTime * (distanceToPredict / givenDistance) * 1.06;
+        }
+    }
+}
